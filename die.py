9ce34d44404d7f7b9da2cd7a1dd454fb2cb868a6
ivanwu <ivanwubusiness@gmail.com>
2023-12-05 11:30:04 -0500

    Final Code

--- die.py
+++ die.py
@@ -2,26 +2,26 @@ import stdio
 import stdrandom
 
 # Set value to a random integer from [1, 6].
-value = stdrandom.uniformInt(..., ...)
+value = stdrandom.uniformInt(1, 7)
 
 # Set output to the empty string.
-...
+output = ""
 
 # Set output to the appropriate string based on value.
 if value == 1:
     output = "     \n  *  \n     "
-elif ...:
-    output = ...
-elif ...:
-    output = ...
-elif ...:
-    output = ...
-elif ...:
-    output = ...
-elif ...:
-    output = ...
+elif value == 2:
+    output = output = "*    \n     \n    *"
+elif value == 3:
+    output = output = "*    \n  *  \n    *"
+elif value == 4:
+    output = output = "*   *\n     \n*   *"
+elif value == 5:
+    output = "*   *\n  *  \n*   *"
+elif value == 6:
+    output = "* * *\n     \n* * *"
 else:
     output = "Error"
 
 # Write output to standard output.
-...
+stdio.writeln(output)
