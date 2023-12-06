06e2113440e5dc7fc8349267e83951f14983636e
ivanwu <ivanwubusiness@gmail.com>
2023-12-06 10:05:11 -0500

    Final Code

--- factorial.py
+++ factorial.py
@@ -7,7 +7,7 @@ n = int(sys.argv[1])
 # Set result to 1.
 result = 1
 
-for i in range(2, n + 1):
+for i in range(1, n + 1):
     # For each value of i from [2, n], set result to its current value times i.
     result = (result * i)
 
